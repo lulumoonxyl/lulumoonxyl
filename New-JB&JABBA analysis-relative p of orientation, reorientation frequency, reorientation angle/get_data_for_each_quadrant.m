d6269d6fbd58a04555728f353099201dfcf0d073
lulumoonxyl <89754082+lulumoonxyl@users.noreply.github.com>
2022-04-01 12:20:57 -0400

    Delete New-JB&JABBA analysis-relative p of orientation, reorientation frequency, reorientation angle directory

--- New-JB&JABBA analysis-relative p of orientation, reorientation frequency, reorientation angle/get_data_for_each_quadrant.m
+++
@@ -1,8 +0,0 @@
-function data_split_q=get_data_for_each_quadrant(data_split,str)
-fn=fieldnames(data_split);
-idx=find(contains(fn,str));
-fn_q=fn(idx);
-for i=1:length(fn_q)
-    data_split_q.(fn_q{i})=data_split.(fn_q{i});
-end 
-end 
